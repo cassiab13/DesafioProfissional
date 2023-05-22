a48ceaa4b4f39a04f5cc1122c7cc091010acb3d2
cassia <ra-22265008-2@alunos.unicesumar.edu.br>
2023-05-22 18:55:48 -0300

    correcao da logo

--- Desenvolvimento_Telas/darkMode.js
+++ Desenvolvimento_Telas/darkMode.js
@@ -2,9 +2,9 @@
 function DarkMode() {
     const body = document.body;
     const logo = document.querySelector('.logo');
-    const menu = document.querySelector(".menu")
-    const logoReservex = document.querySelector('.logo_reservex')
-    // const header = document.querySelector('.header')
+    const menu = document.querySelector('.menu');
+    const logoReservex = document.querySelector('.logo_reservex');
+
     // Verifica se o modo escuro já está ativado
     const isDarkMode = body.classList.contains('dark-mode');
   
@@ -14,17 +14,16 @@ function DarkMode() {
       logo.classList.remove('dark-mode')
       menu.classList.remove('dark-mode')
       logoReservex.classList.remove('dark-mode')
-    //   header.classList.remove('dark-mode')
+
     } else {
       // Ativa
       body.classList.add('dark-mode');
       logo.classList.add('dark-mode')
       menu.classList.add('dark-mode')
       logoReservex.classList.add('dark-mode')
-    //   header.classList.add('dark-mode')
     }
   }
   
-  // Adiciona o evento de clique ao botão de alternância
+  // Botão Dark Mode
   const darkModeButton = document.getElementById('darkMode');
   darkModeButton.addEventListener('click', DarkMode);
