3a1148010b50dd8e7a6a527ed23fc98007235ad1
cassia <ra-22265008-2@alunos.unicesumar.edu.br>
2023-06-13 19:10:23 -0300

    1 tentativa de implementacao do dark na logo

--- Desenvolvimento_Telas/darkMode.js
+++ Desenvolvimento_Telas/darkMode.js
@@ -1,29 +1,53 @@
 // Função para alternar o modo claro e escuro
 function DarkMode() {
-    const body = document.body;
-    const logo = document.querySelector('.logo');
-    const menu = document.querySelector('.menu');
-    const logoReservex = document.querySelector('.logo_reservex');
-    
-    // Verifica se o modo escuro já está ativado
-    const isDarkMode = body.classList.contains('dark-mode');
-  
-    // Desativa
-    if (isDarkMode) {
-      body.classList.remove('dark-mode');
-      logo.classList.remove('dark-mode')
-      menu.classList.remove('dark-mode')
-      logoReservex.classList.remove('dark-mode')
+  const body = document.body;
+  const logo = document.querySelector('.logo');
+  const menu = document.querySelector('.menu');
+  const logoReservex = document.querySelector('.logo_reservex');
+  const darkModeButton = document.getElementById('toggleDarkMode');
 
-    } else {
-      // Ativa
-      body.classList.add('dark-mode');
-      logo.classList.add('dark-mode')
-      menu.classList.add('dark-mode')
-      logoReservex.classList.add('dark-mode')
-    }
+  // Verifica se o modo escuro já está ativado
+  const isDarkMode = body.classList.contains('dark-mode');
+
+  // Desativa
+  if (isDarkMode) {
+    body.classList.remove('dark-mode');
+    logo.classList.remove('dark-mode');
+    menu.classList.remove('dark-mode');
+    logoReservex.classList.remove('dark-mode');
+    darkModeButton.src = "../images/MaterialSymbolsDarkModeOutline.svg";
+  } else {
+    // Ativa
+    body.classList.add('dark-mode');
+    logo.classList.add('dark-mode');
+    menu.classList.add('dark-mode');
+    logoReservex.classList.add('dark-mode');
+    darkModeButton.src = "../images/MaterialSymbolsLightModeOutline.svg";
   }
+
+  // DarkLogo(!isDarkMode);
+}
+
+// Botão Dark Mode
+const darkModeButton = document.getElementById('toggleDarkMode');
+darkModeButton.addEventListener('click', DarkMode);
+darkModeButton.addEventListener('click', DarkLogo);
+
+// Função para alterar a logo
+// function DarkLogo(isDarkMode) {
+//   console.log("Entrei na função")
+//   const logoReservex = document.getElementById('logoReservex');
   
-  // Botão Dark Mode
-  const darkModeButton = document.getElementById('darkMode');
-  darkModeButton.addEventListener('click', DarkMode);
+//   if (isDarkMode) {
+//     logoReservex.src = "../images/Logo_dark.jpeg";
+//   } else {
+//     logoReservex.src = "../images/LogoFundo.png";
+//   }
+// }
+
+function DarkLogo() {
+  console.log("Entrei na função")
+  const logoReservex = document.getElementById('logoReservex');
+    logoReservex.src = "../images/Logo_dark.jpeg";
+}
+
